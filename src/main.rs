47e933a134d5812f3f74210267ae4b6d001ebb02
Robert Czechowski <robert@code-intelligence.com>
2022-11-22 18:04:24 +0100

    Squashed commit of the following:
    commit 57c4f4e50e81c78ccc4cfbbd9bffaecd1fcc912e
    Author: Robert Czechowski <robert@code-intelligence.com>
    Date:   Tue Nov 22 18:01:59 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:src/main.rs
    
    commit 74431d437662e534141edb853e6fdbc2f3cb787c
    Author: Markus Zoppelt <markus.zoppelt@code-intelligence.com>
    Date:   Tue Nov 22 17:55:33 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:src/main.rs
    
    commit e71f49379534f3c410d9eaf3960ae95bebe99fb4
    Author: Robert Czechowski <robert@code-intelligence.com>
    Date:   Tue Nov 22 17:47:47 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:src/main.rs
    
    commit fa1fddfe416814fe0ce4fee6556e4f4c05e56318
    Author: Markus Zoppelt <markus.zoppelt@code-intelligence.com>
    Date:   Tue Nov 22 17:40:00 2022 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:src/main.rs
    
    Co-authored-by: Markus Zoppelt <markus.zoppelt@code-intelligence.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,99 @@
+
+fn transition(command: char, x: i32, y:i32, direction: char) -> (i32, i32, char) {
+    match command {
+        'f' => match direction {
+            'n' => (x, y+1, direction),
+            's' => (x, y-1, direction),
+            'e' => (x+1, y, direction),
+            'w' => (x-1, y, direction),
+            _ => panic!("unknown direction!")
+
+        },
+        'l' => match direction {
+            'n' => (x, y, 'w'),
+            's' => (x, y, 'e'),
+            'e' => (x, y, 'n'),
+            'w' => (x, y, 's'),
+            _ => panic!("unknown direction!")
+        },
+
+        _ => unimplemented!()
+    }
+}
+
+fn sequence(commands: &str, x: i32, y:i32, direction: char) -> (i32, i32, char) {
+    let (mut cx, mut cy, mut cd) = (x, y, direction);
+
+    for c in commands.chars() {
+        (cx, cy, cd) = transition(c, cx, cy, cd);
+    }
+    (cx, cy, cd)
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+// create test
+#[cfg(test)]
+mod tests {
+    use crate::transition;
+    use crate::sequence;
+
+    #[test]
+    fn test_transition() {
+        let test_x = 1;
+        let test_y = 2;
+        let test_command = 'f';
+        let test_direction = 'e';
+
+        let (r_x, r_y, r_dir) = transition(test_command, test_x, test_y, test_direction);
+
+        assert_eq!(r_x, 2);
+        assert_eq!(r_y, 2);
+        assert_eq!(r_dir, 'e');
+    }
+
+    #[test]
+    fn test_turn_left() {
+        let test_x = 1;
+        let test_y = 2;
+        let test_command = 'l';
+        let test_direction = 'e';
+
+        let (r_x, r_y, r_dir) = transition(test_command, test_x, test_y, test_direction);
+
+        assert_eq!(r_x, 1);
+        assert_eq!(r_y, 2);
+        assert_eq!(r_dir, 'n');
+
+
+        let test_x = 1;
+        let test_y = 2;
+        let test_command = 'l';
+        let test_direction = 'n';
+
+        let (r_x, r_y, r_dir) = transition(test_command, test_x, test_y, test_direction);
+
+        assert_eq!(r_x, 1);
+        assert_eq!(r_y, 2);
+        assert_eq!(r_dir, 'w');
+    }
+
+    #[test]
+    fn test_sequence() {
+        let commands = "flflf";
+
+        let test_x = 1;
+        let test_y = 2;
+        let test_direction = 'n';
+
+        let result = sequence(commands, test_x, test_y, test_direction);
+
+        assert_eq!(result.0, 0);
+        assert_eq!(result.1, 2);
+        assert_eq!(result.2, 's');
+
+
+    }
 }
